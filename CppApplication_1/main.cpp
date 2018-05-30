c2dbaa439473666ffc0ef402f9057a906df2145d
Yeonwoo Lee <ylee100@u.rochester.edu>
2018-05-30 16:52:25 -0400

    check

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Yeonwoo
+ *
+ * Created on May 30, 2018, 4:46 PM
+ */
+
+#include <cstdlib>
+using namespace std;
+int main(int argc, char** argv) {
+    return 0;
+}
+
