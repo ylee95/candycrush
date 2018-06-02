fbd3f0561051c47fbe618a9efd251b7620cf4ba3
Donghyeon Kim <donghyeon.kim@tacocube.com>
2018-06-02 17:15:07 -0400

    Add Drawing Methods to SdlWrapper Class
    SDL_Window and SDL_Renderer object managed by SdlWrapper should be inside a member object of SdlWrapper, because they need to be initialized after SDL_Init and destroyed before SDL_Quit.
    
    Add Methods
    clear
    drawRect
    fillRect

--- src/sdlwrapper.cpp
+++ src/sdlwrapper.cpp
@@ -9,12 +9,51 @@ SdlWrapper::~SdlWrapper() {
 	SDL_Quit();
 }
 
-bool SdlWrapper::initialize() {
+bool SdlWrapper::initialize(const std::string &title, int width, int height) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		// Initialize failed.
 		return false;
 	}
 	else {
 		// Initialize success.
+		// Create a window.
+		graphics.window = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>(SDL_CreateWindow(title.data(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0), SDL_DestroyWindow);
+		// Create a renderer.
+		graphics.renderer = std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>(SDL_CreateRenderer(graphics.window.get(), -1, SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer);
+
+		if (graphics.window.get() == nullptr || graphics.renderer.get() == nullptr) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
+}
+
+void SdlWrapper::clear(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	SDL_SetRenderDrawColor(graphics.renderer.get(), r, g, b, a);
+	SDL_RenderClear(graphics.renderer.get());
+}
+
+void SdlWrapper::drawRect(int x, int y, int w, int h, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	SDL_SetRenderDrawColor(graphics.renderer.get(), r, g, b, a);
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	rect.w = w;
+	rect.h = h;
+	SDL_RenderDrawRect(graphics.renderer.get(), &rect);
+}
+
+void SdlWrapper::fillRect(int x, int y, int w, int h, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	SDL_SetRenderDrawColor(graphics.renderer.get(), r, g, b, a);
+	SDL_Rect rect;
+	rect.x = x;
+	rect.y = y;
+	rect.w = w;
+	rect.h = h;
+	SDL_RenderFillRect(graphics.renderer.get(), &rect);
 }
--- src/sdlwrapper.h
+++ src/sdlwrapper.h
@@ -1,6 +1,9 @@
 #ifndef CANDYCRUSH_SDLWRAPPER_H
 #define CANDCANDYCRUSH_SDLWRAPPER_H
 #include <SDL.h>
+#include <cstdint>
+#include <memory>
+#include <string>
 
 class SdlWrapper {
 public:
@@ -14,10 +17,31 @@ public:
 	// Allow move constructor.
 	SdlWrapper(SdlWrapper &&) = default;
 
-	bool initialize();
+	// SDL wrapper instance should be initialized before using.
+	// There is no need for cleanup code, because it is handled automatically on destruction.
+	// Initialization is not included inside the constructor to allow control over window creation.
+	bool initialize(const std::string &title, int width, int height);
+
+	// Clear rendering target.
+	void clear(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+	// Draw a rectangle.
+	void drawRect(int x, int y, int w, int h, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+	// Fill a rectangle.
+	void fillRect(int x, int y, int w, int h, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 
 private:
 	bool initialized;
+
+	// Private constructor.
 	SdlWrapper();
+
+	// Object holding SDL window and renderer.
+	// SDL_Window and SDL_Renderer needs to reside inside a member object of SdlWrapper class, because they need to be initialized after SDL_Init and
+	// destroyed before SDL_Quit.
+	struct {
+		// Initialize window and renderer with nullptr.
+		std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>(nullptr, SDL_DestroyWindow);
+		std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> renderer = std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>(nullptr, SDL_DestroyRenderer);
+	} graphics;
 };
 #endif // CANDYCRUSH_SDLWRAPPER_H
